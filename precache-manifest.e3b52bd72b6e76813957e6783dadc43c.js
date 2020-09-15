3c815c47ac38e35feff8a9a19155b34b2f4acd5d
cSyu2611 <littlemoneyseeker@gmail.com>
2020-09-15 15:02:30 +0900

    Updates

---
+++ precache-manifest.e3b52bd72b6e76813957e6783dadc43c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fc17f8ccf091858c7abb806e3ad32d3",
+    "url": "/react-typing-app-prototype/index.html"
+  },
+  {
+    "revision": "7cc00368c2bd0eaa7fa7",
+    "url": "/react-typing-app-prototype/static/css/main.47512b36.chunk.css"
+  },
+  {
+    "revision": "ccd70d7f867c1c3d4f34",
+    "url": "/react-typing-app-prototype/static/js/2.bb83cd07.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-typing-app-prototype/static/js/2.bb83cd07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cc00368c2bd0eaa7fa7",
+    "url": "/react-typing-app-prototype/static/js/main.7eb9fb58.chunk.js"
+  },
+  {
+    "revision": "105c26f424638ac77b64",
+    "url": "/react-typing-app-prototype/static/js/runtime-main.a90b88f9.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.340fb2f6b01ecd47a5e5fb5b8d2f5542.js"
+  "/react-typing-app-prototype/precache-manifest.e3b52bd72b6e76813957e6783dadc43c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-typing-app-prototype/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
